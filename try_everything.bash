e55fdcbe1aa5fa8bc8307896b2d00e44e711df20
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-13 19:23:24 -0400

    Update script

--- try_everything.bash
+++ try_everything.bash
@@ -11,7 +11,7 @@ node classic-package/vanilla.js
 node classic-package/vanilla.cjs
 node module-package/esm.js
 node module-package/vanilla.cjs
-node -e 'require("ai-jsx")'
+node -e 'console.log(require("ai-jsx"))'
 
 rm -rf dist
 
