891b66551c6ec5602ee243d3f2e63e9e5463d2c1
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-22 17:43:02 -0700

    Reshuffling and things work again

--- try_everything.bash
+++ try_everything.bash
@@ -2,28 +2,28 @@
 
 set -x
 
-echo "=== Expected to fail because the requesting file is invalid ==="
-set +e
-node classic-package/esm.js
-node module-package/vanilla.js
-set -e
-
 echo "=== Should work ==="
 node classic-package/vanilla.js
 node classic-package/vanilla.cjs
 node module-package/esm.js
 node module-package/vanilla.cjs
 node -e 'console.log(require("ai-jsx"))'
-npx tsc -p classic-package/tsconfig-output-to-esm-resolution-node.json
+# npx tsc -p classic-package/tsconfig-output-to-esm-resolution-node.json
 
 rm -rf */dist
+
+echo "=== Should work ==="
+npx tsc -p module-package/tsconfig-output-to-esm.json
+npx tsc -p module-package/tsconfig-output-to-cjs.json
+
+echo "=== Expected to fail because the requesting file is invalid ==="
+set +e
+node classic-package/esm.js
+node module-package/vanilla.js
+set -e
 
 echo "=== Expected to fail ==="
 set +e
 npx tsc -p classic-package/tsconfig-output-to-esm.json
 npx tsc -p classic-package/tsconfig-output-to-cjs.json
 set -e
-
-echo "=== Should work ==="
-npx tsc -p module-package/tsconfig-output-to-esm.json
-npx tsc -p module-package/tsconfig-output-to-cjs.json
